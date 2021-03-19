c39a3686ca94727070d123318ffa34c26233f237
Brian Seidl <brian.seidl@coxautoinc.com>
2021-03-19 00:11:36 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home";
+
+class App extends React.Component {
+  /**
+   * React render function.
+   *
+   * Preconditions:
+   *   - Status of the component is updated.
+   * Postconditions:
+   *   - Component is rendered on page with latest data.
+   */
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route exact path={"/"} component={Home} />
+          {/* TODO: add routes here */}
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
