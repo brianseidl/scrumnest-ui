ee081357dfa58b957e26c366cfc9e26afad39178
Brian Seidl <brian.seidl@coxautoinc.com>
2021-04-19 16:44:18 -0400

    fix button

--- src/components/Dialogs/CreateStoryModal.js
+++ src/components/Dialogs/CreateStoryModal.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/button";
+import Button from "react-bootstrap/Button";
 
 import { API, graphqlOperation } from "aws-amplify";
 
