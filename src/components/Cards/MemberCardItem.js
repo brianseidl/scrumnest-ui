05defc6d7a0a8f8ce7e931daddcc94203ec0d7e4
Derrick Persaud <derrickpersaud@mail.adelphi.edu>
2021-04-27 23:22:53 -0400

    deleting a member or pending member implemented removed commented code

--- src/components/Cards/MemberCardItem.js
+++ src/components/Cards/MemberCardItem.js
@@ -14,7 +14,6 @@ const MemberCardItem = ({ memberItem, onClicked }) => {
           class="close float-right"
           aria-label="Close"
           display="inline-block"
-          // onClick={() => onClicked(memberItem)}
           onClick={() => onClicked(memberItem)}
         >
           <span aria-hidden="true">&times;</span>
